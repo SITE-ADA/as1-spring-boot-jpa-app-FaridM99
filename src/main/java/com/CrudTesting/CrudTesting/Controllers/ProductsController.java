b15fa62ce51481e8de88bd882ec3289b49599b04
FaridM99 <mehdiyev_2002@mail.ru>
2024-03-23 21:38:18 +0400

    Controller has been added

---
+++ src/main/java/com/CrudTesting/CrudTesting/Controllers/ProductsController.java
@@ -0,0 +1,29 @@
+package com.CrudTesting.CrudTesting.Controllers;
+
+import com.CrudTesting.CrudTesting.models.Product;
+import com.CrudTesting.CrudTesting.services.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+public class ProductsController {
+
+
+    @Autowired
+    private ProductsRepository repo;
+
+    @GetMapping({"", "/"})
+    public String showProductList(Model model) {
+
+        List<Product> products = repo.findAll();
+        model.addAttribute("products", products);
+        return "products/index";
+    }
+
+}
