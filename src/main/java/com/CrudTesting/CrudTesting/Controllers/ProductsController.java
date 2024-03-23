9458dbdb98105a02814257e89f653fe09cb263c8
FaridM99 <mehdiyev_2002@mail.ru>
2024-03-23 22:07:06 +0400

    Here, i have added descending order of the products, but i will delete it, it is just for fun

--- src/main/java/com/CrudTesting/CrudTesting/Controllers/ProductsController.java
+++ src/main/java/com/CrudTesting/CrudTesting/Controllers/ProductsController.java
@@ -3,6 +3,7 @@ package com.CrudTesting.CrudTesting.Controllers;
 import com.CrudTesting.CrudTesting.models.Product;
 import com.CrudTesting.CrudTesting.services.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +22,7 @@ public class ProductsController {
     @GetMapping({"", "/"})
     public String showProductList(Model model) {
 
-        List<Product> products = repo.findAll();
+        List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products", products);
         return "products/index";
     }
