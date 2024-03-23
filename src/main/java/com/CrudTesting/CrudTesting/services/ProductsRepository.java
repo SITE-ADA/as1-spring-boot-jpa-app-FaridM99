ddc9aed594434eeb311955367f85eaf24a85d976
FaridM99 <mehdiyev_2002@mail.ru>
2024-03-23 21:34:18 +0400

    ProductsRepository  added

---
+++ src/main/java/com/CrudTesting/CrudTesting/services/ProductsRepository.java
@@ -0,0 +1,7 @@
+package com.CrudTesting.CrudTesting.services;
+
+import com.CrudTesting.CrudTesting.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductsRepository extends JpaRepository <Product, Integer> {
+}
